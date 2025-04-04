106c39d4504451add2769d5b4a911d896463ddc7
TaliaJudy <152074334+TaliaJudy@users.noreply.github.com>
2025-04-04 08:24:14 +0300

    Update index.html

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
 <html>
-  <style>
-    body {
-     background-image: url('https://telegra.ph/file/409fcacdda459445da6eb.png');
-      background-position: fixed;
-      background-image: url('https://files.catbox.moe/7fpahe.jpeg')
-    }
-    
-    p {
-      font-size: 155px;
-      
-    }
-  </style>
-  <body>
-    <p><u>BEAUTY EXISTS 😻</u></p>
-    
-  <img src= "https://files.catbox.moe/qbqv62.jpg">
-    <hr>
- <img src=" https://files.catbox.moe/cbgfu9.jpg">
-  <hr>
-  <img src= "https://files.catbox.moe/ujwonf.jpg">
-    <hr>
-  <img src = "https://files.catbox.moe/oiwm3i.jpg">
-    <hr>
-  </body>
-  <p>2025 ¢ytra</p>
+<body>
+<style>
+body {
+background-color: yellow;
+}
+</style>
+<p align="center">
+
+
+  <a href="https://wa.link/iet9zr">
+    <img src="https://img.shields.io/badge/CLICK HERE-red?style=for-the-badge&logo=whatsapp" 
+    alt="Follow for Updates" width="300"/>
+  </a>
+</p>
+</body>
 </html>
-  
